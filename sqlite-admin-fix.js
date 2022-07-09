4b31ca00d3135c0a70d1e61eb1a3e075e096cd55
Nicholas Westerhausen <nwesterhausen@gmail.com>
2022-07-09 12:16:54 -0400

    feat: ✨ script to force an admin user on sqlite

---
+++ sqlite-admin-fix.js
@@ -0,0 +1,15 @@
+/**
+ * This file can be run with node to make a user admin in the sqlite database (testing database)
+ */
+
+const user_id = 1; // Typcially first user created 
+const permission_id = 3; // ADMIN permissions
+const database_filename = "dev-db.sqlite3"; // name of database (dev)
+//const database_filename = "db.sqlite3"; // name of database (prod)
+
+const db = require("better-sqlite3")(database_filename, {
+    fileMustExist: true,
+});
+
+const row = db.prepare(`INSERT INTO user_permissions (user_id, permission_id) values (${user_id}, ${permission_id})`).run();
+console.dir(row);
