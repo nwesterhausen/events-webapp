fd812c55e270ad5a831a263521bea1bf55920d12
Nicholas Westerhausen <nwesterhausen@gmail.com>
2022-07-09 17:05:35 -0400

    feat: ✨ handle logout

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,7 +1,22 @@
-import express, { Router } from 'express';
+import express, { RequestHandler, Router } from 'express';
 import { getAccount } from './account';
+import debugLib from 'debug';
+
+const debug = debugLib('eventsapp:auth');
+
+const LogoutHandler: RequestHandler = (req, res) => {
+  if (req.session.user) {
+    debug(`destroying session for user ${req.session.user}`);
+    req.session.destroy(() => {
+      debug('Session destroyed');
+    });
+  }
+  res.redirect(302, '/');
+};
+
 const router = express.Router();
 
 router.get('/me', getAccount);
+router.get('/logout', LogoutHandler);
 
 export default router;
