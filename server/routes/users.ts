0f761631167dde82f5af4785d35d2356348db6ae
Nicholas Westerhausen <nwesterhausen@gmail.com>
2022-07-10 06:44:30 -0400

    fix: 🚑️ add awaits

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -4,8 +4,8 @@ import { Knex } from 'knex';
 export const getUsers: RequestHandler = (req, res) => {
   new Promise(async (resolve, reject) => {
     const knex = req.app.get('db') as Knex;
-    const users = knex('users').select();
-    const permissions = knex('user_permissions').select();
+    const users = await knex('users').select();
+    const permissions = await knex('user_permissions').select();
 
     resolve(
       res.status(200).send({
